d25a3bc19f581072617dbf343e1f76bed5fef9c2
Wafflepotato <karimshura@gmail.com>
2023-01-03 15:34:19 +0100

    Authentication and checking if the user exists

--- server/src/Routes/userController.js
+++ server/src/Routes/userController.js
@@ -1,7 +1,48 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../Models/User");
 const Exercise = require("../Models/Exercise");
+
+router.use(cors());
+
+//authentication
+router.get("/posts", authenticateToken, (req, res) => {
+    res.send("Authenticated");
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({
+        Username: req.body.Username,
+        Password: req.body.Password,
+    });
+    if (user) {
+        const authUser = { name: user.Username, pass: user.Password };
+
+        const accessToken = generateAccessToken(authUser);
+        res.json({ accessToken: accessToken });
+    } else {
+        res.sendStatus(300);
+    }
+});
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.SECRET_ACCESS_TOKEN);
+}
 
 router.post("/create", async (req, res) => {
     await User.create({
@@ -11,6 +52,17 @@ router.post("/create", async (req, res) => {
     });
     res.status(200).send("User Created");
 });
+
+router.get("/login", async (req, res) => {
+    const user = await User.findOne({
+        Username: req.body.Username,
+        Password: req.body.Password,
+    });
+    res.send(user);
+    res.status(200).send("User Found");
+});
+
+//----------------------------------------------------------
 
 //requirements 35 & 36
 router.get("/startexam", async (req, res) => {
@@ -33,47 +85,59 @@ router.put(":username/solveexam", async (req, res) => {
 });
 
 router.put("/:username/changeCountry", async (req, res) => {
-    await User.updateOne({ Username: req.params.username }, { $set: {Country: req.body.country }});
+    await User.updateOne(
+        { Username: req.params.username },
+        { $set: { Country: req.body.country } }
+    );
     res.status(200).send("Country Updated");
 });
 
 router.post("/changePass", async (req, res) => {
     const userDetails = await User.findOne({ Username: req.body.Username });
-    if (userDetails.Password === req.body.Password){
+    if (userDetails.Password === req.body.Password) {
         res.status(200).send("Please type a new password");
-    }
-    else{
-        await User.updateOne({ Username: req.body.Username }, { $set: {Password: req.body.Password }});
+    } else {
+        await User.updateOne(
+            { Username: req.body.Username },
+            { $set: { Password: req.body.Password } }
+        );
         res.status(200).send("Password changed successfully");
     }
 });
 //req 33 NOT TESTED PROPERLY
 router.post("/:user/rateInstructor", async (req, res) => {
-    await User.updateOne({ Username: req.body.Username}, { $set: {Rating: req.body.Rating }});
+    await User.updateOne(
+        { Username: req.body.Username },
+        { $set: { Rating: req.body.Rating } }
+    );
     // res.send(await User.find({Username: req.params.user}))
     res.status(200).send("Rating changed successfully");
-}
-);
+});
 
 // Requirements 29 NOT TESTED PROPERLY
 router.put("/:user/edit", async (req, res) => {
-    await User.updateOne({ Username: req.body.Username }, { $set: {Biography: req.body.Biography }});
-    await User.updateOne({ Username: req.body.Username }, { $set: {Email: req.body.Email }});
+    await User.updateOne(
+        { Username: req.body.Username },
+        { $set: { Biography: req.body.Biography } }
+    );
+    await User.updateOne(
+        { Username: req.body.Username },
+        { $set: { Email: req.body.Email } }
+    );
     res.send("Biography and/or email updated successfully");
 });
 
-
 router.post("/changePass", async (req, res) => {
     const userDetails = await User.findOne({ Username: req.body.Username });
-    if (userDetails.Password === req.body.Password){
+    if (userDetails.Password === req.body.Password) {
         res.status(200).send("Please type a new password");
-    }
-    else{
-        await User.updateOne({ Username: req.body.Username }, { $set: {Password: req.body.Password }});
+    } else {
+        await User.updateOne(
+            { Username: req.body.Username },
+            { $set: { Password: req.body.Password } }
+        );
         res.status(200).send("Password changed successfully");
     }
 });
-
-
 
 module.exports = router;
