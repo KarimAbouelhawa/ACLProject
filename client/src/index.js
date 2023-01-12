70107ae4bb1e0c118ee66cb46d3af3fc99196223
Adel Eltoweiry <adel.eltoweiry15@gmail.com>
2023-01-12 21:35:35 +0100

    navigate1

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "./Styles/index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+//import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+//import Page2 from "./page2"
+//import Login2 from "./Pages/Login2";
+import SignUp from "./Pages/SignUp";
+//import history from "./history"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Router>
+    <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+    </Routes>
+</Router>);
