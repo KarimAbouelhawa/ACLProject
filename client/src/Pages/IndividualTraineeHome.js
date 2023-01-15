75f5529f1f57423dac0f24d2d1679fc7ba89e1e4
Deathstroke1411 <mt22815@gmail.com>
2023-01-15 20:27:02 +0100

    Home Page

--- client/src/Pages/IndividualTraineeHome.js
+++ client/src/Pages/IndividualTraineeHome.js
@@ -1,31 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
+import axios from "axios";
+import CourseComponent from "../Components/CourseComponent";
 import "../Styles/IndividualTraineeHomeStyles.css";
 
 function IndividualTraineeHome() {
+    const [courses, setCourses] = React.useState([]);
+
+    React.useEffect(() => {
+        async function getCourses(){
+            const res = await axios.get("http://localhost:8000/course/");
+            const courses = res.data;
+            const courseComponents = courses.map(course => (<CourseComponent {...course}/>))
+            setCourses(courseComponents);
+        }
+        getCourses();
+    }, [])
+
+
     return(
 
-        <div class = "Page">
-            <div class = "Header">
+        <div className = "Page">
+            <div className = "Header">
             <img src={require("../ACL_Logo.png")} alt="Logo" width="315" height="100"></img>
             
             <img src={require("../profile.png")} alt="Logo" width="120" height="70"></img>
             </div>
 
-            <div class  = "LeftPanel">
+            <div className  = "LeftPanel">
                 <h1 id = "text1"><u>Search</u></h1>
-                <h4 class = "searchTitles">By Subject:</h4>
+                <h4 className = "searchTitles">By Subject:</h4>
                  <input
                     type="text"
                     name="SearchBarSubject"
                     size="40"
                 />
-                <h4 class = "searchTitles">By Title:</h4>
+                <h4 className = "searchTitles">By Title:</h4>
                 <input
                     type="text"
                     name="SearchBarTitle"
                     size="40"
                 />
-                <h4 class = "searchTitles">By Instructor:</h4>
+                <h4 className = "searchTitles">By Instructor:</h4>
                 <input
                     type="text"
                     name="SearchBarInstructor"
@@ -37,15 +52,16 @@ function IndividualTraineeHome() {
                     name="SearchBarRating"
                     size="40"
                 />
-                <h4 class = "searchTitles">By Price:</h4>
+                <h4 className = "searchTitles">By Price:</h4>
                 <input
                     type="text"
                     name="SearchBarInstructor"
                     size="40"
                 />
             </div>
-            <div class = "MiddlePanel">
+            <div className = "MiddlePanel">
                 <h1>Popular Courses: </h1>
+                {courses}
                 <h1>My Courses: </h1>
             </div>
         </div>
