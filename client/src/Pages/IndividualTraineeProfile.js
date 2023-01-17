6a9cd383fe7da09cfeaf41532e49f5ff0a2e848d
Wafflepotato <karimshura@gmail.com>
2023-01-17 16:44:28 +0100

    Instructor course search and route to correct homepage

--- client/src/Pages/IndividualTraineeProfile.js
+++ client/src/Pages/IndividualTraineeProfile.js
@@ -6,17 +6,19 @@ import TraineeCourseComponent from "../Components/TraineeCourseComponent";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-
 function IndividualTraineeProfile() {
-
     const [courses, setCourses] = React.useState([]);
-    const [cookies, setCookie] = useCookies(["user"]);
+    const [cookies, removeCookie] = useCookies(["user"]);
 
-    console.log(cookies.user.Username)
+    console.log(cookies.user.Username);
 
     React.useEffect(() => {
         async function getCourses() {
-            const res = await axios.get("http://localhost:8000/user/" + cookies.user.Username + "/userCourses");
+            const res = await axios.get(
+                "http://localhost:8000/user/" +
+                    cookies.user.Username +
+                    "/userCourses"
+            );
             const courses = res.data;
             const courseComponents = courses.map(course => (
                 <TraineeCourseComponent {...course} />
@@ -27,43 +29,59 @@ function IndividualTraineeProfile() {
         getCourses();
     }, []);
 
-    return(
-    <div>
-        <div className="Header">
-        <a href="/IndividualTraineeHome">
-                <img src={require("../ACL_Logo.png")} alt="Logo" width="315" height="100"></img>
-        </a>        
-                <Link to="/" className= "logout">
-                    <button className ="signout">Sign Out</button>
+    return (
+        <div>
+            <div className="Header">
+                <a href="/IndividualTraineeHome">
+                    <img
+                        src={require("../ACL_Logo.png")}
+                        alt="Logo"
+                        width="315"
+                        height="100"
+                    ></img>
+                </a>
+                <Link to="/" className="logout">
+                    <button
+                        className="signout"
+                        onClick={() => {
+                            removeCookie("user", { path: "/" });
+                        }}
+                    >
+                        Sign Out
+                    </button>
                 </Link>
                 <Link to="/IndividualTraineeChangePassword">
-                    <button className = "changepassword">Change Password</button>
+                    <button className="changepassword">Change Password</button>
                 </Link>
-
-
             </div>
-        <div>
-            <center>
-                <div id="profileContainer">
-                    <br></br>
-
-                    <img name = "profilePic" src={require("../profileBlack.png")} alt="Logo" width="250" height="250"></img>
-                    <h1>{cookies.user.Username}</h1>
-                    <Link to="/IndividualTraineePayment">
-                        <button className="paymentdetails">Payment Details</button>
-                    </Link>
-                    <h3>My Wallet:</h3>
-                    <h2>0</h2>
-                    <br></br>
-                </div>
-                <div>
-                    <h1>My Courses:</h1>
-                </div>
-            </center>
             <div>
-            {courses}
+                <center>
+                    <div id="profileContainer">
+                        <br></br>
+
+                        <img
+                            name="profilePic"
+                            src={require("../profileBlack.png")}
+                            alt="Logo"
+                            width="250"
+                            height="250"
+                        ></img>
+                        <h1>{cookies.user.Username}</h1>
+                        <Link to="/IndividualTraineePayment">
+                            <button className="paymentdetails">
+                                Payment Details
+                            </button>
+                        </Link>
+                        <h3>My Wallet:</h3>
+                        <h2>0</h2>
+                        <br></br>
+                    </div>
+                    <div>
+                        <h1>My Courses:</h1>
+                    </div>
+                </center>
+                <div>{courses}</div>
             </div>
-        </div>
         </div>
     );
 }
--- client/src/Pages/InstructorProfile.js
+++ client/src/Pages/InstructorProfile.js
@@ -7,13 +7,15 @@ import axios from "axios";
 
 function InstructorProfile() {
     const [courses, setCourses] = React.useState([]);
-    const [cookies, setCookie] = useCookies(["user"]);
-
-    console.log(cookies.user.Username)
+    const [cookies, removeCookie] = useCookies(["user"]);
+    const [formData, setFormData] = useState([]);
 
     React.useEffect(() => {
         async function getCourses() {
-            const url = "http://localhost:8000/instructor/" + cookies.user.Username + "/courses";
+            const url =
+                "http://localhost:8000/instructor/" +
+                cookies.user.Username +
+                "/courses";
             const res = await axios.get(url);
             console.log(url);
             const courses = res.data;
@@ -26,12 +28,45 @@ function InstructorProfile() {
         getCourses();
     }, []);
 
+    function handleChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const searchResult = await axios.post(
+                `http://localhost:8000/instructor/${cookies.user.Username}/courses`,
+                formData
+            );
+            const courses = searchResult.data;
+            const courseComponents = courses.map(course => (
+                <InstructorCourseComponent {...course} />
+            ));
+            setCourses(courseComponents);
+        } catch (error) {
+            console.log("error");
+        }
+    }
+
     return (
         <div>
             <div className="Header">
-                <img src={require("../ACL_Logo.png")} alt="Logo" width="315" height="100"></img>
+                <img
+                    src={require("../ACL_Logo.png")}
+                    alt="Logo"
+                    width="315"
+                    height="100"
+                ></img>
                 <Link to="/" className="logout">
-                    <button className="signout">Sign Out</button>
+                    <button
+                        className="signout"
+                        onClick={() => {
+                            removeCookie("user", { path: "/" });
+                        }}
+                    >
+                        Sign Out
+                    </button>
                 </Link>
                 <Link to="/IndividualTraineeChangePassword">
                     <button className="changepassword">Change Password</button>
@@ -41,24 +76,64 @@ function InstructorProfile() {
                 <center>
                     <div id="profileContainer">
                         <br></br>
-                        <img name="profilePic" src={require("../profile.png")} alt="Logo" width="250" height="250"></img>
+                        <img
+                            name="profilePic"
+                            src={require("../profile.png")}
+                            alt="Logo"
+                            width="250"
+                            height="250"
+                        ></img>
                         <h1>{cookies.user.Username}</h1>
                         <h2>{cookies.user.Email}</h2>
                         <h2>{cookies.user.Biography}</h2>
                         <Link to="/InstructorEdit">
-                            <button className="paymentdetails">Change Biography/Email</button>
+                            <button className="paymentdetails">
+                                Change Biography/Email
+                            </button>
                         </Link>
                         <br></br>
                     </div>
                     <div>
                         <h1>My Courses:</h1>
                     </div>
+                    <div id="Search">
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                id="searchboxes"
+                                placeholder="Title"
+                                type="text"
+                                name="Title"
+                                value={formData.Title}
+                                onChange={handleChange}
+                            />
+                            <input
+                                id="searchboxes"
+                                placeholder="Subject"
+                                type="text"
+                                name="Subject"
+                                value={formData.Subject}
+                                onChange={handleChange}
+                            />
+                            <input
+                                id="searchboxes"
+                                placeholder="Price"
+                                type="number"
+                                name="Price"
+                                value={formData.Price}
+                                onChange={handleChange}
+                            />
+                            <center>
+                                <button
+                                    className="searchbutton"
+                                    formAction="post"
+                                >
+                                    Search
+                                </button>
+                            </center>
+                        </form>
+                    </div>
                 </center>
-                <div>
-
-                    {courses}
-
-                </div>
+                <div>{courses}</div>
             </div>
         </div>
     );
