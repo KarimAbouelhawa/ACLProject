9fba2c2a7644dee0aed9052364a63e0a7dceadbb
Wafflepotato <karimshura@gmail.com>
2023-01-18 00:00:01 +0100

    last minute

--- client/src/Pages/CourseExercise.js
+++ client/src/Pages/CourseExercise.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "../Styles/CreateCourse.css";
 import { Cookies, useCookies } from "react-cookie";
 import SubtitleFormComponent from "../Components/SubtitleFormComponent";
 
 function CourseExercise() {
     const navigate = useNavigate();
+    const params = useParams();
     const cookies = useCookies(["user"]);
     const [errorMessage, setErrorMessage] = useState("");
-    const [formData, setFormData] = useState({
-        Subtitles: [],
-        Exercise: [],
-    });
-    const [subtitleComponents, setSubtitleComponents] = React.useState([]);
-    const [subtitleData, setSubtitleData] = React.useState([]);
-    const [subtitleNumber, setSubtitleNumber] = useState(1);
+    const [formData, setFormData] = useState({});
+    const [exercise, setExercise] = useState([]);
+
+    React.useEffect(() => {
+        async function getCourse() {
+            const res = await axios.get(
+                `http://localhost:8000/course/${params.coursename}`
+            );
+            setExercise(res.data[0].Exercise);
+            console.log(res.data[0].Exercise);
+        }
+        getCourse();
+    }, []);
 
     function handleChange(event) {
         const { name, value, type, checked } = event.target;
@@ -27,60 +35,7 @@ function CourseExercise() {
         });
     }
 
-    function addSubtitle() {
-        const currentSubtitles = [...subtitleComponents];
-        currentSubtitles.push(
-            <SubtitleFormComponent
-                key={subtitleNumber}
-                number={subtitleNumber}
-                changeData={newData =>
-                    changeSubtitleData(subtitleNumber, newData)
-                }
-            />
-        );
-        setSubtitleComponents(currentSubtitles);
-        setSubtitleNumber(subtitleNumber + 1);
-    }
-
-    function changeSubtitleData(subtitleNumber, thisSubtitleData) {
-        const currentSubtitleData = [...subtitleData];
-        currentSubtitleData[subtitleNumber - 1] = thisSubtitleData;
-        setSubtitleData(currentSubtitleData);
-    }
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-        formData.Instructor = cookies[0].user.Username;
-        formData.Hours = 0;
-        formData.Rating = 0;
-        for (let i = 0; i < subtitleData.length; i++) {
-            formData.Subtitles.push(subtitleData[i]);
-            formData.Hours = formData.Hours + subtitleData[i].Length;
-        }
-        const newForm = formData;
-        newForm.Exercise.push(formData.ExerciseQuestion);
-        newForm.Exercise.push(formData.ExerciseAnswer1);
-        newForm.Exercise.push(formData.ExerciseAnswer2);
-        newForm.Exercise.push(formData.ExerciseAnswer3);
-        newForm.Exercise.push(formData.ExerciseAnswer4);
-        newForm.Exercise.push(formData.CorrectAnswer);
-        setFormData(newForm);
-        console.log(formData.Exercise);
-        if (formData.AgreementContract) {
-            try {
-                const userData = await axios.post(
-                    "http://localhost:8000/course/create",
-                    formData
-                );
-                console.log("data sent successfully.");
-                navigate("/InstructorProfile");
-            } catch (error) {
-                setErrorMessage("Incomplete Data");
-            }
-        } else {
-            setErrorMessage("You must agree on the contract");
-        }
-    }
+    async function handleSubmit(e) {}
 
     return (
         <div id="paymentContainer">
